c5e407adc211b4e1bd28eaa49958c626fb702fdc
T. Alexander <t.alex@outlook.com>
2014-05-08 09:22:25 -0400

    Update FlabbyManuel.java
    Hitboxes now work, improved GUI wording, implemented high score system, shows twinkies on final frame

--- testprograms/FlabbyManuel.java
+++ testprograms/FlabbyManuel.java
@@ -1,6 +1,9 @@
-import java.util.ArrayList;
+import java.io.File; import java.io.FileNotFoundException; import java.io.PrintWriter; import java.util.ArrayList;
+import java.util.Scanner;
 public class FlabbyManuel {
-	public static void main(String args[]) throws InterruptedException {
+	public static void main(String args[]) throws Exception {
+		final int jumpHeight = 12;
+		final double gravity = .8;
 		Graphics.x = new Graphics();
 		while (true) {
 		double deltaY = 1;
@@ -8,7 +11,7 @@ public class FlabbyManuel {
 		double yPos = 300;
 		int textureCount = 0, mainCount = 0;
 	//FPS Monitoring Variables
-		int stTime = (int) System.currentTimeMillis(), fps = 0, frameCount = 0, nOfValues = 0, sumOfFps = 0;
+		int stTime = (int) System.currentTimeMillis(), fps = 0, frameCount = 0;
 	//Introduction Game Loop
 		boolean introRunning = true;
 		while (introRunning) {
@@ -27,12 +30,6 @@ public class FlabbyManuel {
 			else if (textureCount == 3) Graphics.drawPic(xPos, yPos, .3, "sprite_manuel/hi.png");
 			Graphics.drawPic(453, 320, 4, "logo.png");
 			Graphics.drawPic(499, 450, .3, "start.png");
-		//Debugging Output
-			if (fps != 0) {
-				Graphics.writeString(0, 0, 2, fps + " FPS");
-				if (nOfValues != 0) Graphics.writeString(0, 30, 2, Math.round(sumOfFps / nOfValues) + " avg");
-				Graphics.writeString(0, 60, 2, textureCount + " tex");
-				Graphics.writeString(0, 90, 1, "Mouse Pos: X=" + Graphics.userMouseX + ", Y=" + Graphics.userMouseY); }
 		//Is the button pressed
 			if (Graphics.mousePressed && Graphics.userMouseX > 504 && Graphics.userMouseY > 478 && Graphics.userMouseX < 781 && Graphics.userMouseY < 548) introRunning = false;
 			Graphics.makeImage();
@@ -45,9 +42,7 @@ public class FlabbyManuel {
 			if (((int) System.currentTimeMillis()) > (stTime + 1000)) {
 				stTime = (int) System.currentTimeMillis();
 				fps = frameCount;
-				frameCount = 0;
-				sumOfFps = sumOfFps + fps;
-				nOfValues++; } }
+				frameCount = 0; } }
 		Graphics.clearScreen();
 		Graphics.makeImage();
 		Thread.sleep(500);
@@ -68,12 +63,6 @@ public class FlabbyManuel {
 				else if (textureCount == 2) Graphics.drawPic(xPos, yPos, .3, "sprite_manuel/mid.png");
 				else if (textureCount == 3) Graphics.drawPic(xPos, yPos, .3, "sprite_manuel/hi.png");
 				Graphics.drawPic(508, 277, .5, "tutorial.png");
-			//Debugging Output
-				if (fps != 0) {
-					Graphics.writeString(0, 0, 2, fps + " FPS");
-					if (nOfValues != 0) Graphics.writeString(0, 30, 2, Math.round(sumOfFps / nOfValues) + " avg");
-					Graphics.writeString(0, 60, 2, textureCount + " tex");
-					Graphics.writeString(0, 90, 1, "Mouse Pos: X=" + Graphics.userMouseX + ", Y=" + Graphics.userMouseY); }
 			//Is the button pressed
 				if (Graphics.mousePressed) tutorialRunning = false;
 				Graphics.makeImage();
@@ -86,22 +75,27 @@ public class FlabbyManuel {
 				if (((int) System.currentTimeMillis()) > (stTime + 1000)) {
 					stTime = (int) System.currentTimeMillis();
 					fps = frameCount;
-					frameCount = 0;
-					sumOfFps = sumOfFps + fps;
-					nOfValues++; } }
+					frameCount = 0; } }
+		
 		boolean gameRunning = true;
 		deltaY = 0;
 		ArrayList<Double> twinkiesX = new ArrayList<Double>();
 		ArrayList<Double> twinkiesTopThreshold = new ArrayList<Double>();
 		int RunningTicksTimer = 0;
 		int score = 0;
+		
+		File inFile = new File("HiScore.dat");
+		Scanner in = new Scanner(inFile);
+		int hiScore = in.nextInt();
+		in.close();
+		
 		while (gameRunning) {
 			//Animated Picture
 				mainCount++;
 				if (mainCount%5 == 0) textureCount++;
 				if (textureCount == 4) textureCount = 0;
 				yPos = yPos - deltaY;
-				deltaY = deltaY - 0.8;
+				deltaY = deltaY - gravity;
 				if (yPos > 630) gameRunning = false;
 				if (RunningTicksTimer%100 == 0) {
 					twinkiesX.add(0, 1446.0);
@@ -110,6 +104,11 @@ public class FlabbyManuel {
 					Graphics.drawPic(twinkiesX.get(i), twinkiesTopThreshold.get(i) - 423, 1, "twinkytop.png");
 					if (twinkiesTopThreshold.get(i) > 423) Graphics.drawPic(twinkiesX.get(i), twinkiesTopThreshold.get(i) - 846, 1, "twinkybottom.png");
 					Graphics.drawPic(twinkiesX.get(i), twinkiesTopThreshold.get(i) + 225, 1, "twinkybottom.png");
+					
+					if (twinkiesX.get(i) < 466 && twinkiesX.get(i) > 134) {
+						if (twinkiesTopThreshold.get(i) - 15 > yPos) gameRunning = false; //precise 0
+						if (twinkiesTopThreshold.get(i) + 240 < yPos) gameRunning = false; } //precise 225
+					
 					twinkiesX.set(i, twinkiesX.get(i) - 5);
 					if (twinkiesX.get(i) < -200) {
 						twinkiesX.remove(i);
@@ -124,14 +123,10 @@ public class FlabbyManuel {
 				else if (textureCount == 3) Graphics.drawPic(xPos, yPos, .3, "sprite_manuel/hi.png");
 			//Debugging Output
 				if (fps != 0) {
-					Graphics.writeString(0, 0, 2, fps + " FPS");
-					if (nOfValues != 0) Graphics.writeString(0, 30, 2, Math.round(sumOfFps / nOfValues) + " avg");
-					Graphics.writeString(0, 60, 2, textureCount + " tex");
-					Graphics.writeString(0, 90, 1, "Mouse Pos: X=" + Graphics.userMouseX + ", Y=" + Graphics.userMouseY);
-					Graphics.writeString(0, 120, 1, RunningTicksTimer + " ticks");
-					Graphics.writeString(0, 150, 1, twinkiesX.size() + ", " + twinkiesTopThreshold.size() + " entities");
-					Graphics.writeString(0, 180, 1, score + " points"); }
-				if (Graphics.mousePressed) deltaY = 12; 
+					//Graphics.writeString(0, 0, 2, fps + " FPS");
+					Graphics.writeString(0, 60, 2, "Score: " + score);
+					Graphics.writeString(0, 90, 2, "High Score: " + hiScore); }
+				if (Graphics.mousePressed) deltaY = jumpHeight;
 				Graphics.makeImage();
 				Graphics.mousePressed = false;
 			//Preparation for Next Frame
@@ -142,18 +137,33 @@ public class FlabbyManuel {
 				if (((int) System.currentTimeMillis()) > (stTime + 1000)) {
 					stTime = (int) System.currentTimeMillis();
 					fps = frameCount;
-					frameCount = 0;
-					sumOfFps = sumOfFps + fps;
-					nOfValues++; }
+					frameCount = 0; }
 				RunningTicksTimer++; }
-		Graphics.writeString(640, 360, 2, "You got The Beetus.");
-		Graphics.writeString(640, 390, 2, "Score " + score);
-		Graphics.writeString(640, 420, 1, "Click Anywhere to Continue");
+	//Draws Manuel for Final Frame
 		if (textureCount == 0) Graphics.drawPic(xPos, yPos, .3, "sprite_manuel/mid.png");
 		else if (textureCount == 1) Graphics.drawPic(xPos, yPos, .3, "sprite_manuel/low.png");
 		else if (textureCount == 2) Graphics.drawPic(xPos, yPos, .3, "sprite_manuel/mid.png");
 		else if (textureCount == 3) Graphics.drawPic(xPos, yPos, .3, "sprite_manuel/hi.png");
+	//Draws Twinkies for Final Frame
+		for (int i = 0; i < twinkiesX.size(); i++) {
+			Graphics.drawPic(twinkiesX.get(i), twinkiesTopThreshold.get(i) - 423, 1, "twinkytop.png");
+			if (twinkiesTopThreshold.get(i) > 423) Graphics.drawPic(twinkiesX.get(i), twinkiesTopThreshold.get(i) - 846, 1, "twinkybottom.png");
+			Graphics.drawPic(twinkiesX.get(i), twinkiesTopThreshold.get(i) + 225, 1, "twinkybottom.png"); }
+	//Text for Final Frame
+		Graphics.writeString(640, 360, 2, "You Lost Your Curves!");
+		if (score > hiScore) Graphics.writeString(640, 390, 2, "New High Score!" + score);
+		else Graphics.writeString(640, 390, 2, "Check your privilege!");
+		Graphics.writeString(640, 420, 2, "Score: " + score + "  High Score: " + hiScore);
+		Graphics.writeString(640, 450, 1, "Click Anywhere to Continue...");
 		Graphics.makeImage();
+	//Saves High Score, if Changed
+		if (score > hiScore) {
+			try {
+				PrintWriter out = new PrintWriter(new File("HiScore.dat"));
+				out.print(score);
+				out.close(); }
+			catch (FileNotFoundException e) {e.printStackTrace(); } }
+	//Wait for Input
 		while (true) {
 			if (Graphics.mousePressed) break;
 			Graphics.mousePressed = false;
