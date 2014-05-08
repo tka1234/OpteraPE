44cc35e9c75b312362627745a3abd9634f3e65a2
T. Alexander <t.alex@outlook.com>
2014-05-08 09:23:30 -0400

    Update Graphics.java
    General cleanup, removed stupid quasi 3D system, added : and ! to the integrated font writer

--- src/Graphics.java
+++ src/Graphics.java
@@ -1,18 +1,14 @@
-import java.awt.*; import java.awt.event.*; import java.awt.geom.Line2D; import java.awt.image.*; import java.io.*; import java.util.*; import javax.imageio.ImageIO; import javax.swing.*;
+import java.awt.*; import java.awt.event.*; import java.awt.geom.Line2D; import java.awt.image.*; import java.util.*; import javax.swing.*;
 public class Graphics extends JFrame implements KeyListener {
 	private static final long serialVersionUID = 1L;
 	public static boolean AntiAliasingOn = false;
-	
 	private static Graphics2D image;
 	private static BufferedImage imageB;
-	
 	private static Object keyLock = new Object();
 	private static TreeSet<Integer> keysDown = new TreeSet<Integer>();
 	private static LinkedList<Character> keysTyped = new LinkedList<Character>();
-	
 	public static Graphics x;
 	public JFrame drawing;
-	
 	public static boolean mousePressed;
 	public static int userMouseX, userMouseY;
 	
@@ -69,6 +65,16 @@ public class Graphics extends JFrame implements KeyListener {
 				image.drawImage(character, (int) Math.round(xPos), (int) (Math.round(yPos)), (int) Math.round(character.getWidth(null) * scale), (int) Math.round(character.getHeight(null) * scale), null);
 				xPos = xPos + (9 * scale);
 				count++; }
+			else if (type.charAt(count) == ":".charAt(0)) {
+				Image character = new ImageIcon("FontChars/colon.png").getImage();
+				image.drawImage(character, (int) Math.round(xPos), (int) (Math.round(yPos)), (int) Math.round(character.getWidth(null) * scale), (int) Math.round(character.getHeight(null) * scale), null);
+				xPos = xPos + (9 * scale);
+				count++; }
+			else if (type.charAt(count) == "!".charAt(0)) {
+				Image character = new ImageIcon("FontChars/exclamation.png").getImage();
+				image.drawImage(character, (int) Math.round(xPos), (int) (Math.round(yPos)), (int) Math.round(character.getWidth(null) * scale), (int) Math.round(character.getHeight(null) * scale), null);
+				xPos = xPos + (9 * scale);
+				count++; }
 			else if (Character.isLowerCase(type.charAt(count))) {
 				Image character = new ImageIcon("FontChars/lc_" + type.charAt(count) + ".png").getImage();
 				image.drawImage(character, (int) Math.round(xPos), (int) (Math.round(yPos) + (3 * scale)), (int) Math.round(character.getWidth(null) * scale), (int) Math.round(character.getHeight(null) * scale), null);
@@ -80,63 +86,13 @@ public class Graphics extends JFrame implements KeyListener {
 				xPos = xPos + (9 * scale);
 				count++; } } }
 	
+	/**Draws a pixel at xPos, yPos with color a**/
 	public static void drawPixel(double xPos, double yPos, Color a) {
 		Color orig = image.getColor();
 		setPen(a);
 		image.fillRect((int) Math.round(xPos), (int) Math.round(yPos), 1, 1);
 		setPen(orig); }
-	
-	@Deprecated
-	public static void YeOldeDistortedPicture(double xPos, double yPos, double scale, String fname, double propLTop, double propLBot, double propRTop, double propRBot) {
-		BufferedImage img = null;
-		try {img = ImageIO.read(new File(fname)); }
-		catch (IOException e) {e.printStackTrace(); }
-		Raster raster = img.getData();
-		double startingYPos = ((1 - propLTop) * raster.getHeight());
-		double currentTProp = propLTop;
-		double currentBProp = propLBot;
-		for (int x = 0; x < raster.getWidth(); x++) {
-			currentTProp = currentTProp + ((propRTop - propLTop) / raster.getWidth());
-			currentBProp = currentBProp + ((propRBot - propLBot) / raster.getWidth());
-			double InstantaneousProportionSize = currentTProp - currentBProp;
-			double countForPersistentPixel = 0;
-			double positionY = startingYPos;
-			
-			for (int y = 0; y < raster.getHeight(); y++) {
-				if (countForPersistentPixel >= 1) {
-					drawPixel(x + xPos, positionY + yPos, new Color(img.getRGB(x, y)));
-					positionY++;
-					countForPersistentPixel--; }
-				else countForPersistentPixel = countForPersistentPixel + InstantaneousProportionSize + .50;
-			}
-			startingYPos = startingYPos - ((propRTop - propLTop) / raster.getWidth()) * raster.getHeight();
-		}
-	}
-	
-	public static void drawDistortedPicture(double xPos, double yPos, double scale, String fname, double propLTop, double propLBot, double propRTop, double propRBot) {
-		try {
-			BufferedImage img = ImageIO.read(new File(fname));
-			Raster raster = img.getData();
-			double topPropThisCol = propLTop;
-			double botPropThisCol = propLBot;
-			double RequiredPixelsThisCol;
-			for (int x = 0; x < raster.getWidth(); x++) {
-				
-				RequiredPixelsThisCol = (botPropThisCol - topPropThisCol) * raster.getHeight();
-				double newY = yPos + (topPropThisCol * raster.getHeight());
-				for (int y = 0; y < RequiredPixelsThisCol; y++) {
-					drawPixel(x + xPos, newY, new Color(img.getRGB(x, y)));
-					newY++;
-				}
-				topPropThisCol = topPropThisCol - ((propRTop - propLTop) / raster.getWidth());
-				botPropThisCol = botPropThisCol + ((propRBot - propLBot) / raster.getWidth());
-			}
-			
-			
-		}
-		catch (IOException E) {E.printStackTrace(); }
-	}
-	
+
 	/**Draw a picture to the display at (xPos, yPos), scaled to scale.**/
 	public static void drawPic(double xPos, double yPos, double scale, String fname) {
 		Image createdImage = new ImageIcon(fname).getImage();
@@ -150,19 +106,6 @@ public class Graphics extends JFrame implements KeyListener {
 	
 	public static void main(String args[]) throws InterruptedException {
 		x = new Graphics();
-		
-		/*int count = 0;
-		while (count < 250) {
-			drawLine(Math.random() * 400, Math.random() * 400, Math.random() * 400, Math.random() * 400);
-			count++; }
-		writeString(10, 500, 2, "My beetus grandplanets were enfuriated at the outrageous mobility of");
-		writeString(10, 530, 2, "Thin Privilege.");
-		writeString(10, 590, 3, "V.L.P. Very Large Person Club");
-		writeString(10, 640, 2, "Special Event. Doors Widen at 10:30");
-		drawPic(50, 50, 1, "testManuel.jpg");
-		drawDistortedPicture(50, 50, 1, "Manuel.jpg", .2, .9, .3, .7);
-		makeImage(); */
-		
 		int mouseCount = 0;
 		while (true) {
 			Thread.sleep(16);
@@ -172,9 +115,4 @@ public class Graphics extends JFrame implements KeyListener {
 			writeString(10, 500, 2, "Mouse Clicked " + mouseCount + " times.");
 			writeString(10, 530, 2, "Mouse Pos: X=" + userMouseX + ", Y=" + userMouseY);
 			makeImage();
-			mousePressed = false;
-			
-			
-		}
-		}
-	}
+			mousePressed = false; } } }
